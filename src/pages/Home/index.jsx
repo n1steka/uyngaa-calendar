b63c30f8138c9944d2ee66d0fd45b074a12e7775
munkherdene <mongolzkarigun@gmail.com>
2024-03-12 11:43:01 +0800

    calendar done

---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,215 @@
+import { useState, useEffect } from 'react';
+import Link from "next/link"
+
+const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+
+function Home( { count }) {
+  const [events, setEvents] = useState([]);
+  const [month, setMonth] = useState(new Date().getMonth());
+  const [year, setYear] = useState(new Date().getFullYear());
+  const [no_of_days, setNoOfDays] = useState([]);
+  const [blankdays, setBlankDays] = useState([]);
+  const [event_title, setEventTitle] = useState('');
+  const [event_date, setEventDate] = useState('');
+  const [event_theme, setEventTheme] = useState('blue');
+  const [openEventModal, setOpenEventModal] = useState(false);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+  const openModal  = () => {
+    setIsMenuOpen(true)
+  }
+  useEffect(() => {
+    const fetchEvents = async () => {
+      try {
+        const res = await fetch('/api/event'); // Assuming your API endpoint is '/api/event'
+        if (res.ok) {
+          const data = await res.json();
+          setEvents(data.data); // Assuming events data is in the 'data' property of the response
+        } else {
+          console.error('Failed to fetch events:', res.statusText);
+        }
+      } catch (error) {
+        console.error('Error fetching events:', error.message);
+      }
+    };
+
+    fetchEvents();
+  }, []);
+
+  useEffect(() => {
+    getNoOfDays();
+  }, [month, year]);
+
+  const initDate = () => {
+    let today = new Date();
+    setMonth(today.getMonth());
+    setYear(today.getFullYear());
+  };
+
+  const isToday = (date) => {
+    const today = new Date();
+    const d = new Date(year, month, date);
+    return today.toDateString() === d.toDateString();
+  };
+
+  const showEventModal = (date) => {
+    setOpenEventModal(true);
+    setEventDate(new Date(year, month, date).toDateString());
+  };
+
+  const addEvent = () => {
+    if (event_title === '') return;
+
+    setEvents([...events, { event_date, event_title, event_theme }]);
+
+    setEventTitle('');
+    setEventDate('');
+    setEventTheme('blue');
+
+    setOpenEventModal(false);
+  };
+
+  const getNoOfDays = () => {
+    let daysInMonth = new Date(year, month + 1, 0).getDate();
+    let dayOfWeek = new Date(year, month).getDay();
+    let blankdaysArray = [];
+    for (let i = 1; i <= dayOfWeek; i++) {
+      blankdaysArray.push(i);
+    }
+
+    let daysArray = [];
+    for (let i = 1; i <= daysInMonth; i++) {
+      daysArray.push(i);
+    }
+
+    // Adjust days for February in leap years
+    if (month === 1 && ((year % 4 === 0 && year % 100 !== 0) || year % 400 === 0)) {
+      daysArray = Array.from({ length: 29 }, (_, i) => i + 1);
+    }
+
+    setBlankDays(blankdaysArray);
+    setNoOfDays(daysArray);
+  };
+
+  return (
+    <>
+    <div  className=' w-full h-[50px] bg-blue-900'>
+    <h1 className='text-white flex h-full  items-center ml-[50px] '> Утас: 99001122  </h1>
+    </div>
+      <nav className="h-24 w-full bg-slate-300">
+        <ul className="flex justify-between p-8">
+          <li className=''>
+            <h1 className="text-2xl mx-[200px">
+              <Link href={"/"}>
+                Нүүр
+              </Link>
+            </h1>
+          </li>
+          <li className='mx-[200px] '>
+            <h1>
+              <Link href={"/login"}>
+                Нэвтрэх
+              </Link>
+            </h1>
+          </li>
+        </ul>
+      </nav>
+      <div className="antialiased sans-serif bg-gray-100 h-screen">
+        <div className="container mx-auto px-4 py-2 md:py-24">
+          <div className="bg-white rounded-lg shadow overflow-hidden">
+            <div className="flex items-center justify-between py-2 px-6">
+              <div>
+                <span className="text-lg font-bold text-gray-800">{MONTH_NAMES[month]}</span>
+                <span className="ml-1 text-lg text-gray-600 font-normal">{year}</span>
+              </div>
+              <div className="border rounded-lg px-1" style={{ paddingTop: '2px' }}>
+                <button
+                  type="button"
+                  className={`leading-none rounded-lg transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-300 p-1 text-black items-center ${month === 0 && 'cursor-not-allowed opacity-25'}`}
+                  disabled={month === 0}
+                  onClick={() => { setMonth( month -1   ); }}
+                >
+                  <svg className="h-6 w-6 text-gray-500 inline-flex leading-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
+                  </svg>
+                </button>
+                <div className="border-r inline-flex h-6"></div>
+                <button
+                  type="button"
+                  className={`leading-none rounded-lg transition ease-in-out duration-100 inline-flex items-center cursor-pointer hover:bg-gray-200 p-1 ${month === 11 && 'cursor-not-allowed opacity-25'}`}
+                  disabled={month === 11}
+                  onClick={() => { setMonth(month +1 ); }}
+                >
+                  <svg className="h-6 w-6 text-gray-500 inline-flex leading-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
+                  </svg>
+                </button>
+              </div>
+            </div>
+         
+      
+            <div className="-mx-1 -mb-1">
+              <div className="flex flex-wrap" style={{ marginBottom: '-40px' }}>
+                {DAYS.map((day, index) => (
+                  <div key={index} style={{ width: '14.26%' }} className="px-2 py-2">
+                    <div className="text-gray-600 text-sm uppercase tracking-wide font-bold text-center">{day}</div>
+                  </div>
+                ))}
+              </div>
+
+              <div className="flex flex-wrap border-t border-l">
+                {blankdays.map((blankday, index) => (
+                  <div key={index} style={{ width: '14.28%', height: '120px' }} className="text-center border-r border-b px-4 pt-2" ></div>
+                ))}
+                {no_of_days.map((date, dateIndex) => (
+                  <div key={dateIndex} style={{ width: '14.28%', height: '120px' }} className="px-4 pt-2 border-r border-b relative text-green-500 ">
+                    <div
+                      onClick={() => showEventModal(date)}
+                      className={`inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100  ${isToday(date) ? 'bg-blue-500 text-black' : 'text-gray-700 hover:bg-blue-300'}`}
+                    >
+                      {date}
+                    </div>
+                    <div style={{ height: '80px' }} className="overflow-y-auto mt-1">
+                      {events.filter(event => new Date(event.event_date).toDateString() === new Date(year, month, date).toDateString()).map((event, index) => (
+                        <div key={index} className={`px-2 py-1 rounded-lg mt-1 overflow-hidden border border-${event.event_theme}-200 text-${event.event_theme}-800 bg-${event.event_theme}-100`}>
+                          <p onClick={openModal}  className="cursor-pointer p-2  text-sm truncate leading-tight">{event.event_title}</p>
+                          {isMenuOpen && (
+                            <div className="fixed inset-0 bg-gray-900 bg-opacity-50 flex justify-center items-center z-50   ">
+                              <div className="bg-white rounded-md p-6 max-w-md w-[500px] h-[500px] ">
+                                <ul className='  p-4'>
+                                  <li>
+                                  <h1> Дэлгэрэнгүй</h1>
+                                  <span className=' text-blue-600'>
+                                  Арга хэмжээ :</span>
+                                  <div className=' p-4 border w-full h-full rounded-md mt-12'>
+                                  <p className="block py-2 px-4 text-gray-800
+                                  hover:bg-gray-200"> {event.event_title}  </p>
+                                  </div>
+                                  </li>
+                              
+                                  </ul>
+                                <button
+                                  className="block mt-4 px-4 py-2 text-gray-800 bg-gray-200 hover:bg-gray-300 rounded-md"
+                                  onClick={() => setIsMenuOpen(false)}
+                                >
+                                  Хаах
+                                </button>
+                              </div>
+                            </div>
+                          )}
+                        </div>
+                      ))}
+                    </div>
+                  </div>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,180 +1,64 @@
-import { useState, useEffect } from 'react';
-import Link from "next/link"
-const MONTH_NAMES = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-const DAYS = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+import { useState } from 'react';
+import Home from './admin';
+import Link from 'next/link';
 
-function Home() {
-  const [events, setEvents] = useState([]);
-
-  useEffect(() => {
-    const fetchEvents = async () => {
-      try {
-        const res = await fetch('/api/event'); // Assuming your API endpoint is '/api/event'
-        if (res.ok) {
-          const data = await res.json();
-          setEvents(data.data); // Assuming events data is in the 'data' property of the response
-        } else {
-          console.error('Failed to fetch events:', res.statusText);
-        }
-      } catch (error) {
-        console.error('Error fetching events:', error.message);
-      }
-    };
-
-    fetchEvents();
-  }, []);
-  const [month, setMonth] = useState(new Date().getMonth());
+function Main() {
   const [year, setYear] = useState(new Date().getFullYear());
-  const [no_of_days, setNoOfDays] = useState([]);
-  const [blankdays, setBlankDays] = useState([]);
-  // const [events, setEvents] = useState([
-  //   { event_date: new Date(2024, 3, 1), event_title: "April Fool's Day", event_theme: 'blue' },
-  //   { event_date: new Date(2024, 3, 10), event_title: "Birthday", event_theme: 'red' },
-  //   { event_date: new Date(2024, 3, 16), event_title: "Upcoming Event", event_theme: 'green' }
-  // ]);
-  const [event_title, setEventTitle] = useState('');
-  const [event_date, setEventDate] = useState('');
-  const [event_theme, setEventTheme] = useState('blue');
-  const [openEventModal, setOpenEventModal] = useState(false);
+  const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+  const dayNames = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
 
-  const initDate = () => {
-    let today = new Date();
-    setMonth(today.getMonth());
-    setYear(today.getFullYear());
-  };
+  function isLeapYear(year) {
+    return (year % 4 === 0 && year % 100 !== 0) || (year % 400 === 0);
+  }
 
-  const isToday = (date) => {
-    const today = new Date();
-    const d = new Date(year, month, date);
-    return today.toDateString() === d.toDateString();
-  };
+  function getDaysInMonth(month, year) {
+    const daysInMonth = [31, isLeapYear(year) ? 29 : 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
+    return daysInMonth[month];
+  }
 
-  const showEventModal = (date) => {
-    setOpenEventModal(true);
-    setEventDate(new Date(year, month, date).toDateString());
-  };
+  function generateCalendar(year) {
+    const calendar = [];
+    for (let month = 0; month < 12; month++) {
+      const monthDays = getDaysInMonth(month, year);
+      const firstDayOfMonth = new Date(year, month, 1).getDay(); // 0-indexed day of the week (0 = Sunday)
 
-  const addEvent = () => {
-    if (event_title === '') return;
-
-    setEvents([...events, { event_date, event_title, event_theme }]);
-
-    setEventTitle('');
-    setEventDate('');
-    setEventTheme('blue');
-
-    setOpenEventModal(false);
-  };
-
-  const getNoOfDays = () => {
-    let daysInMonth = new Date(year, month + 1, 0).getDate();
-    let dayOfWeek = new Date(year, month).getDay();
-    let blankdaysArray = [];
-    for (let i = 1; i <= dayOfWeek; i++) {
-      blankdaysArray.push(i);
+      const monthGrid = [];
+      for (let i = 0; i < firstDayOfMonth; i++) {
+        monthGrid.push('');
+      }
+      for (let day = 1; day <= monthDays; day++) {
+        monthGrid.push(day);
+      }
+      calendar.push(monthGrid);
     }
+    return calendar;
+  }
 
-    let daysArray = [];
-    for (let i = 1; i <= daysInMonth; i++) {
-      daysArray.push(i);
-    }
-
-    setBlankDays(blankdaysArray);
-    setNoOfDays(daysArray);
-  };
+  const calendar = generateCalendar(year);
 
   return (
-    <>
-      <nav className="h-24  w-full bg-slate-300">
-        <ul className="flex  justify-between p-8">
-          <li className=''>
-            <h1 className="text-2xl mx-[200px">
-              <Link href={"/"}>
-                Нүүр
-              </Link>
-            </h1>
-          </li>
-          <li className='mx-[200px] '>
-            <h1>
-              <Link href={"/login"}>
-                Нэвтрэх
-              </Link>
-            </h1>
-          </li>
-        </ul>
-      </nav>
-      <div className="antialiased sans-serif bg-gray-100 h-screen">
-        <div className="container mx-auto px-4 py-2 md:py-24">
-          <div className="bg-white rounded-lg shadow overflow-hidden">
-            <div className="flex items-center justify-between py-2 px-6">
-              <div>
-                <span className="text-lg font-bold text-gray-800">{MONTH_NAMES[month]}</span>
-                <span className="ml-1 text-lg text-gray-600 font-normal">{year}</span>
-              </div>
-              <div className="border rounded-lg px-1" style={{ paddingTop: '2px' }}>
-                <button
-                  type="button"
-                  className={`leading-none rounded-lg transition ease-in-out duration-100 inline-flex cursor-pointer hover:bg-gray-300 p-1  text-black  items-center ${month === 0 && 'cursor-not-allowed opacity-25'}`}
-                  disabled={month === 0}
-                  onClick={() => { setMonth(month - 1); getNoOfDays(); }}
-                >
-                  <svg className="h-6 w-6 text-gray-500 inline-flex leading-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M15 19l-7-7 7-7" />
-                  </svg>
-                </button>
-                <div className="border-r inline-flex h-6"></div>
-                <button
-                  type="button"
-                  className={`leading-none rounded-lg transition ease-in-out duration-100 inline-flex items-center cursor-pointer hover:bg-gray-200 p-1 ${month === 11 && 'cursor-not-allowed opacity-25'}`}
-                  disabled={month === 11}
-                  onClick={() => { setMonth(month + 1); getNoOfDays(); }}
-                >
-                  <svg className="h-6 w-6 text-gray-500 inline-flex leading-none" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                    <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 5l7 7-7 7" />
-                  </svg>
-                </button>
-              </div>
-            </div>
-
-            <div className="-mx-1 -mb-1">
-              <div className="flex flex-wrap" style={{ marginBottom: '-40px' }}>
-                {DAYS.map((day, index) => (
-                  <div key={index} style={{ width: '14.26%' }} className="px-2 py-2">
-                    <div className="text-gray-600 text-sm uppercase tracking-wide font-bold text-center">{day}</div>
-                  </div>
-                ))}
-              </div>
-
-              <div className="flex flex-wrap border-t border-l">
-                {blankdays.map((blankday, index) => (
-                  <div key={index} style={{ width: '14.28%', height: '120px' }} className="text-center border-r border-b px-4 pt-2 " ></div>
-                ))}
-                {no_of_days.map((date, dateIndex) => (
-                  <div key={dateIndex} style={{ width: '14.28%', height: '120px' }} className="px-4 pt-2 border-r border-b relative text-green-500 ">
-                    <div
-                      onClick={() => showEventModal(date)}
-                      className={`inline-flex w-6 h-6 items-center justify-center cursor-pointer text-center leading-none rounded-full transition ease-in-out duration-100  ${isToday(date) ? 'bg-blue-500 text-black' : 'text-gray-700 hover:bg-blue-300'}`}
-                    >
-                      {date}
-                    </div>
-                    <div style={{ height: '80px' }} className="overflow-y-auto mt-1">
-                      {events.filter(event => new Date(event.event_date).toDateString() === new Date(year, month, date).toDateString()).map((event, index) => (
-                        <div key={index} className={`px-2 py-1 rounded-lg mt-1 overflow-hidden border border-${event.event_theme}-200 text-${event.event_theme}-800 bg-${event.event_theme}-100`}>
-                          <p className="text-sm truncate leading-tight">{event.event_title}</p>
-                        </div>
-                      ))}
-                    </div>
-                  </div>
-                ))}
-              </div>
+    <div className="container mx-auto p-12  m-12">
+    
+      <h1 className="text-3xl font-bold mb-4">{year} Calendar</h1>
+      <div className="grid grid-cols-3 gap-4">
+        {calendar.map((month, index) => (
+          <div key={index}  className="border p-4 rounded ">
+          <h2 className="text-xl font-semibold mb-2">{monthNames[index]}</h2>
+          <Link href={`/Home/${index}`}  className='underline  cursor-pointer text-red-500'> - Дэлгэрэнгүй -</Link>
+            <div className='p-2 m-2 text-blue-600'> Сар  : {index +1}  </div>
+            <div className="grid grid-cols-7 gap-2">
+              {dayNames.map((day, dayIndex) => (
+                <div key={dayIndex} className="text-sm text-center font-semibold">{day}</div>
+              ))}
+              {month.map((date, dateIndex) => (
+                <div key={dateIndex} className={`text-center ${date === '' ? 'text-gray-300' : ''}`}>{date}</div>
+              ))}
             </div>
           </div>
-        </div>
-
-        ч
-      </div >
-    </>
+        ))}
+      </div>
+    </div>
   );
 }
 
-export default Home;
+export default Main;
